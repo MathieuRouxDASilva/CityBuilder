3308e6e5979c220ea391f0c36c1361d7be3337af
mathieu roux <mathieurouxdasilva2005@gmail.com>
2024-05-15 12:37:17 +0200

    tests

---
+++ core/include/maths/vec2i.h
@@ -0,0 +1,47 @@
+#ifndef CORE_MATHS_VEC2I_H_
+#define CORE_MATHS_VEC2I_H_
+
+namespace core
+{
+	/**
+	 * \brief Vec2i is a struct that allow basic maths
+	 */
+	struct Vec2i
+{
+    int x = 0, y = 0;
+
+    //const means that x and y will not be changed
+    constexpr Vec2i operator+(Vec2i other)const //constexpr -> operator+'s result is calculated in compile time -> make it easier
+    {
+        return {x + other.x , y + other.y};
+    }
+
+    constexpr Vec2i operator-(Vec2i other)const
+	{
+        return {x - other.x, y - other.y };
+    }
+
+    constexpr Vec2i operator-()const
+    {
+        return {-x, -y};
+    }
+
+    static constexpr int Dot(Vec2i v1, Vec2i v2)
+    {
+        return v1.x * v2.x + v1.y * v2.y;
+    }
+
+    constexpr Vec2i Perpendicular() const
+    {
+        return { -y, x };
+    }
+
+
+
+}; //struct Vec2i
+
+
+
+} //namespace core
+
+ #endif //CORE_MATHS_VEC2I_H_
